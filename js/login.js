41d4599e102c79eacc8332aae361d99027ef4e70
matiacuna <matiasacunabene@gmail.com>
2021-08-18 09:16:05 -0300

    entrega 1

--- js/login.js
+++ js/login.js
@@ -1,6 +1,17 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(e){    
+    const loginForm = document.getElementById('signin');
+    //definir un evento para cuando se envie el formulario
+    loginForm.onsubmit= function(e) {
+        e.preventDefault();
+        //guardar el email en localst
+        let userEmail= document.getElementById('userEmail').value;
+        // console.log(userEmail)
+        localStorage.setItem('email', userEmail);
+        //redireccionando a la portada
+        window.location.href = 'index.html';
+    };
 
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,60 @@
+var productArray = [];
+
+function showProductList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let product = array[i];
+
+        htmlContentToAppend += `
+            
+        <div class="row">
+            <div class="col-3">
+                <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+            </div>
+            <div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1">` + product.name + `</h4>
+                    <small class="text-muted">` + product.soldCount + ` artículos</small>
+                    
+                </div>
+                <p class="mb-1">` + product.description + `</p><br>
+                <p><b> ` + product.currency + `-` + product.cost + `</b><p>
+            </div>
+        </div>
+        <hr>
+    
+    `
+
+        document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
 
+
+document.addEventListener("DOMContentLoaded", function(e){
+
+    
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+
+        if (resultObj.status === "ok") {
+            productArray = resultObj.data;
+            showProductList(productArray);
+
+        }
+    
+        
+       
+    });
 });
+
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+//document.addEventListener("DOMContentLoaded", function (e) {});
