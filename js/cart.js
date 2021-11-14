5f9bfad0201e5654078e5736515b1239e38cf8e7
matiacuna <matiasacunabene@gmail.com>
2021-11-14 14:13:44 -0300

    e7

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,16 @@
 var cartProducts = [];
 var altProductsUrl = 'https://matiacuna.github.io/jsons/articles.json'
 
+var nameE = ""
+var ageE = ""
+var addressE = ""
+
+var nameC = ""
+var cardN = ""    
+var cardCode = ""
+
+var bankAc = ""
+
 // función paar mostrar artículos en el carrito
 function showCart(array){
     let htmlContent = "";
@@ -41,8 +51,20 @@ function calcTotal(){
     for (let i = 0; i < subTotals.length; i++){
         total += parseInt(subTotals[i].innerHTML);
     }
-    document.getElementById("total").innerHTML = total;
-    
+
+    //calculo metodo de envio
+    if(document.getElementById("premium").checked){ 
+        total = (total * 0.15) + total;        
+    }
+    if(document.getElementById("express").checked){ 
+        total = (total * 0.07) + total;
+    }
+    if(document.getElementById("standard").checked){ 
+        total = (total * 0.05) + total;
+    }
+
+    //imprimo total
+    document.getElementById("total").innerHTML = total
 }
 
 // función para calcula el costo sub total
@@ -54,7 +76,64 @@ function calcSubTotal(count, i){
     calcTotal();
 }
 
+//funcion apra validar si se ingrearon los datos necesarios para realizar al compra
+function valData(){
+    //datos de usuario
+    nameE = document.getElementById("nameE").value;
+    ageE = document.getElementById("ageE").value;
+    addressE = document.getElementById("addressE").value;
 
+    //datos de targeta
+    nameC = document.getElementById("nameC").value;
+    cardN = document.getElementById("cardN").value;
+    cardCode = document.getElementById("cardCode").value;
+
+    //datos banco
+    bankAc = document.getElementById("bankAc").value;
+
+    let itsOk1 = ""
+    let itsOk2 = ""
+
+    //valido si se ingresaron datos personales y lanzo un alert si no es asi
+    if( nameE === "" || ageE === "" || addressE === ""){
+        //alert("Antes de terminar, por favor completa tus datos personales")
+        document.getElementById("alert1").innerHTML = ` <div class="alert alert-warning alert-dismissible fade show" role="alert" ><strong>Antes de terminar</strong>, por favor completa tus datos personales.
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>  </div>`
+        itsOk1 = "1"
+
+    };
+
+    //valido si se ingresaron datos de pago y lanzo un alert si no es asi
+    if( (nameC === "" || cardN === "" || cardCode === "") && bankAc === "" ){
+        //alert("Antes de terminar, por favor completa la forma de pago")
+        document.getElementById("alert2").innerHTML = ` <div class="alert alert-warning alert-dismissible fade show" role="alert" ><strong>Antes de terminar</strong>, por favor completa la forma de pago.
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>  </div>`
+        itsOk2 = "1"
+
+    };
+
+    //valido si estan todos los datos completos y lanzo el mensaje de que la compra se realizo con exito
+    if( itsOk1 ==! "1" && itsOk2 ==! "1" ) {
+        //alert("Antes de terminar, por favor completa la forma de pago")
+        document.getElementById("alert3").innerHTML = `<div class="alert alert-success" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+        <h1 class="alert-heading">Compra realizada con exito!</h1>
+        <hr>
+        <p class="mb-0">Te mantendremos al tanto del proceso del proceso de envio a traves de email, estate atento a tu correo!</p>
+        
+      </div>`
+    };
+
+
+
+
+}
 
 
 
@@ -71,6 +150,14 @@ document.addEventListener("DOMContentLoaded", function(e){
             calcTotal();
             
         }
+    });
+
+    document.getElementById("shipA").addEventListener("click", function(){ //cuando se seleeciona otro metodo de envio se actualiza el total
+        calcTotal();
+    });
+
+    document.getElementById("buy").addEventListener("click", function(){
+        valData();
     });
 
 
